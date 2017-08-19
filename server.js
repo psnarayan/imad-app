4eec00e166122a605915318640afeca9ff741571
psnarayan <30700415+psnarayan@users.noreply.github.com>
2017-08-19 22:24:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,18 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+var articleOne = {
+    title: 'Article one | Narayan',
+    heading: 'Article one',
+    date: 'August 17, 2017',
+    content: `<p>This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article.</p>
+                
+                <p>This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article.</p>
+                
+                <p>This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article.</p>`
+};
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
